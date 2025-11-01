6e36e1ff512fcea05fe495fbcef45fc826dfcde0
Logan J <loganmj.2015@gmail.com>
2025-11-01 18:33:36 -0400

    Lab 10 Done

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -24,6 +24,7 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -31,7 +32,7 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
 
